b729b30fb54b0fe8c362a7333fc594b83ee47b4a
Kumaran <sathiyavarathankumaran@gmail.com>
2021-08-22 19:43:22 +0530

    first commit

---
+++ app/src/main/java/mad/example/playerlab4/MainActivity.java
@@ -0,0 +1,38 @@
+package mad.example.playerlab4;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public View btnStart;
+    public View btnStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MediaPlayer player = MediaPlayer.create(this, Settings.System.DEFAULT_NOTIFICATION_URI);
+        player.setLooping(true);
+        player.start();
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+
+
+    }
+}
